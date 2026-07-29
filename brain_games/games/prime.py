9d7be99a2572596d58797fc63fb3f3e8a2d6aa35
k.bryzgin <k.bryzgin@yandex.ru>
2026-07-29 21:52:18 +0700

    feat: add brain_prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,11 +7,9 @@ RULES = """Answer "yes" if given number is prime. Otherwise answer "no"."""
 def is_prime(number):
     if number < 2:
         return False
-    
-    for i in range(2, number):
+    for i in range(2, int(number ** 0.5) + 1):
         if number % i == 0:
             return False
-            
     return True
 
 
