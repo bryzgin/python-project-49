4ce81b56d32b2ee141f7eb8f9663dbaea55168da
k.bryzgin <k.bryzgin@yandex.ru>
2026-07-29 21:36:22 +0700

    feat: add brain_gcd

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,24 @@
+import math
+import random
+from brain_games.engine import run_game
+
+RULES = "Find the greatest common divisor of given numbers."
+
+
+def generate_round():
+    num1 = random.randint(1, 100)
+    num2 = random.randint(1, 100)
+    
+    question = f"{num1} {num2}"
+    
+    correct_answer = math.gcd(num1, num2)
+
+    return question, str(correct_answer)
+
+
+def main():
+    run_game(RULES, generate_round)
+
+
+if __name__ == "__main__":
+    main()
